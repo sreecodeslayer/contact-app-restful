ab1ec699f09c35bb99c6067ed5086b71fbea9eff
Sreenadh TC <kesav.tc8@gmail.com>
2018-12-05 22:26:28 +0530

    Setup a contacts model for storing all the contacts

---
+++ contacts/models/records.py
@@ -0,0 +1,18 @@
+from ..extensions import db
+from datetime import datetime
+
+
+class Records(db.Model):
+    __tablename__ = 'contacts'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    surname = db.Column(db.String(100), nullable=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    mobile = db.Column(db.String(20), nullable=True, unique=True)
+    created_on = db.Column(db.DateTime(), default=datetime.utcnow)
+
+    def __init__(self, **kwargs):
+        super(Records, self).__init__(**kwargs)
+
+    def __repr__(self):
+        return '(Record : %r)' % self.name
