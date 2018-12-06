a8d9a84b4834931624293d08c33f4b480b52f000
Sreenadh TC <kesav.tc8@gmail.com>
2018-12-07 00:55:45 +0530

    Add unit test for search

--- tests/test_records_model.py
+++ tests/test_records_model.py
@@ -43,3 +43,68 @@ class ContactModelTestCase(unittest.TestCase):
         self.assertTrue(rec.mobile is not None)
         self.assertTrue(rec.user is not None)
         self.assertTrue(isinstance(rec.created_on, datetime))
+
+    def test_search_contact(self):
+        cat = Records(
+            name='Cat',
+            surname='Cat',
+            email='cat@animals.com',
+            mobile='+911123456789'
+        )
+        cat.user = self.user
+        dog = Records(
+            name='Dog',
+            surname='Dog',
+            email='dog@animals.com',
+            mobile='+912123456789'
+        )
+        dog.user = self.user
+        snake = Records(
+            name='Snake',
+            surname='Snake',
+            email='snake@reptiles.com',
+            mobile='+913123456789'
+        )
+        snake.user = self.user
+        db.session.add(cat)
+        db.session.add(dog)
+        db.session.add(snake)
+
+        db.session.commit()
+
+        # Predominantly search name field
+        catq = Records.query.filter(
+            (Records.name.contains('cat')) | (
+                Records.email.contains('cat')
+            )
+        )
+        self.assertListEqual(list(catq), [cat])
+
+        dogq = Records.query.filter(
+            (Records.name.contains('dog')) | (
+                Records.email.contains('dog')
+            )
+        )
+        self.assertListEqual(list(dogq), [dog])
+
+        snakeq = Records.query.filter(
+            (Records.name.contains('snake')) | (
+                Records.email.contains('snake')
+            )
+        )
+        self.assertListEqual(list(snakeq), [snake])
+
+        # Predominantly search email field
+        animalsq = Records.query.filter(
+            (Records.name.contains('animals')) | (
+                Records.email.contains('animals')
+            )
+        )
+        self.assertListEqual(list(animalsq), [cat, dog])
+
+        reptilesq = Records.query.filter(
+            (Records.name.contains('reptiles')) | (
+                Records.email.contains('reptiles')
+            )
+        )
+        self.assertListEqual(list(reptilesq), [snake])
